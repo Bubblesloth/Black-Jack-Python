411b7ab5d12533f1c7afed98609828685e027bfe
DeltaOG <mestra.matheo@icloud.com>
2024-12-16 09:50:32 +0100

    d

---
+++ Card.py
@@ -0,0 +1,8 @@
+class Card:
+    def _init_(self,rank,value,symbole):
+        self.rank = rank
+        self.value = value
+        self.symbol = symbol
+
+
+
--- Carte.py
+++
@@ -1,8 +0,0 @@
-class Carte:
-    def _init_(self,rank,valeur,symbole):
-        self.rank = rank
-        self.valeur = valeur
-        self.symbole = valeur
-
-
-
