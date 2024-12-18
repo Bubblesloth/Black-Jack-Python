89b6beac8dcc7bc35fd0f60719701d65bd763936
Bubblesloth <74314220+Bubblesloth@users.noreply.github.com>
2024-12-18 15:03:24 +0100

    Deck avancement

--- Card.py
+++ Card.py
@@ -1,5 +1,5 @@
 class Card:
-    def _init_(self,rank,symbol):
+    def __init__(self,rank,symbol):
         self.rank = rank
         try: self.value = int(rank)
         except:
@@ -7,6 +7,7 @@ class Card:
                 self.value = 10
             else: self.value = 'As'
         self.symbol = symbol
+
 
 
 
