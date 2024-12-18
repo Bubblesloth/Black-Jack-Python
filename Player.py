abf8d39c9bd423fc640908ad253306cdc40de134
DeltaOG <mestra.matheo@icloud.com>
2024-12-18 15:27:26 +0100

    q

--- Player.py
+++ Player.py
@@ -1,10 +1,16 @@
 import pile
+import Deck
 class Player:
 
     def __init__(self):
         self.hand = []
 
-    def tirer():
+    def tirer(self):
+        rep = input("Croupier - Carte à {vos points} ?(y/n)")
+        if rep == y:
+            self.Deck.defausser()
+
+
 
 
 
