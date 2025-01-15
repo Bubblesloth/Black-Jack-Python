b559ba8252e1cde0268035104cf0d0345874c0eb
Bubblesloth <lucas.lionnet151@orange.fr>
2025-01-15 15:14:22 +0100

    Lecroupier pioche

--- Black Jack Games.py
+++ Black Jack Games.py
@@ -7,18 +7,23 @@ class BlackJack:
     player1 = Player()
     deck = Deck()
 
+    print('------')
     print(player1.getHand())
-
     player1.tirer(deck)
-
     print(player1.getHand())
-
+    print('------')
     player1.tirer(deck)
-
     print(player1.getHand())
-
+    print('------')
     print(player1.getHand()[0])
+    print('------')
     print(player1.getHand()[1])
+
+    print('Début de la partie\n')
+
+    while croupier.score < 17:
+        croupier.tirer(deck)
+        print(f'La croupier a tiré {croupier.getHand()[croupier.lendeck-1]}, il a désormais {croupier.score} points\n')
 
 
 
--- Player.py
+++ Player.py
@@ -4,10 +4,13 @@ class Player:
     def __init__(self):
         self.hand = []
         self.score = 0
+        self.points = 50
+        self.lendeck = 0
 
     def tirer(self,deck):
         carte = deck.defausser()
         self.hand.append(carte)
+        self.lendeck += 1
         if carte.value != 'As':
             self.score += carte.value
         elif self.score + 11 <= 21:
