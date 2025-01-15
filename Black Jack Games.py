99216146c203261df588fb5dce1f1b21a28b2c77
Bubblesloth <lucas.lionnet151@orange.fr>
2025-01-15 15:20:37 +0100

    Update Black Jack Games.py

--- Black Jack Games.py
+++ Black Jack Games.py
@@ -3,10 +3,13 @@ from Player import *
 
 class BlackJack:
 
+    #Init
     croupier = Player()
     player1 = Player()
     deck = Deck()
+    partie = True
 
+    #Tests
     print('------')
     print(player1.getHand())
     player1.tirer(deck)
@@ -19,11 +22,20 @@ class BlackJack:
     print('------')
     print(player1.getHand()[1])
 
-    print('Début de la partie\n')
+    #Game
+    while partie==True :
+        print('Début de la partie\n')
 
-    while croupier.score < 17:
-        croupier.tirer(deck)
-        print(f'La croupier a tiré {croupier.getHand()[croupier.lendeck-1]}, il a désormais {croupier.score} points\n')
+        while croupier.score < 17:
+            croupier.tirer(deck)
+            print(f'La croupier a tiré {croupier.getHand()[croupier.lendeck-1]}, il a désormais {croupier.score} points\n')
+
+        if croupier.score == 21:
+            partie = False
+
+    print('Partie Finie')
+
+
 
 
 
